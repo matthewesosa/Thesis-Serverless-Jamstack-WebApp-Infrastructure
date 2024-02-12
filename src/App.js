d048b8840a1d457602055d5344f0bb7864afa2e4
Matthew Igbinehi <matthewesosa@gmail.com>
2024-02-12 10:54:03 +0100

    update pn App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React start up
         </a>
       </header>
     </div>
