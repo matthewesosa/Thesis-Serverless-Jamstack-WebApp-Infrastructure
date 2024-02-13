f0ea53a3caed036c2e5123d69f54a5823dbc6ded
Matthew Igbinehi <matthewesosa@gmail.com>
2024-02-13 14:04:38 +0100

    define components Welcome and HotelInfo

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './Welcome.js'
+import HoltelInfo from './HotelInfo.js'
+
+const Main = () => {
+  return (
+  <main id="wrapper">
+    <Welcome />
+    <HoltelInfo />
+  </main>
+
+  );
+}
+
+export default Main;
